8eb404af6d25df8b2fcc799f1df049a68a9db834
Israel Fraga <israelaze@gmail.com>
2022-01-06 11:12:04 -0300

    Update: Instant ISO 8601

--- hr-worker/src/main/java/com/devsuperior/hrworker/exceptions/StandardError.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/exceptions/StandardError.java
@@ -3,9 +3,13 @@ package com.devsuperior.hrworker.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
